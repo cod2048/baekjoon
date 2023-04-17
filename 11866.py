3bb3c739b81bcae7f8c5f1bebfc69300c981dcb4
cod2048 <cod2048@gmail.com>
2023-04-17 19:29:26 +0900

    baekjoon 4/17

--- 11866.py
+++ 11866.py
@@ -1,12 +1,20 @@
-import sys
 from collections import deque
-input = sys.stdin.readline
 
-n, k = input().split()
-people = [i for i in range(1, int(n)+1)]
-result =[]
+n, k = map(int, input().split())
 
-def rm_people(x):
-    
+ysps = []
+queue = deque()
 
-print(people)
+for i in range(1, n + 1):
+    queue.append(i)
+
+while queue:
+    for i in range(k - 1):
+        queue.append(queue.popleft())
+
+    ysps.append(queue.popleft())
+
+print('<', end="")
+for i in range(len(ysps)-1):
+    print(ysps[i], end= ', ')
+print(ysps[-1], end=">")
