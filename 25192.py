e6a1c007d7bd5c0813dc0d6da4a3da8ebc8e554b
cod2048 <cod2048@gmail.com>
2023-05-15 10:48:57 +0900

    baekjoon 5/15

--- 25192.py
+++ 25192.py
@@ -1,10 +1,13 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
 
 chatLog = set()
 cnt = 0
 
 for i in range(n):
-    chat = input()
+    chat = input().strip()
     if chat != "ENTER":
         chatLog.add(chat)
     else:
