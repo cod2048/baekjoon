27bef5fa16156d5c48efc7c117ad4c6da2b02d40
cod2048 <cod2048@gmail.com>
2023-06-07 08:54:38 +0900

    수 정렬하기, 버블정렬

--- 2750.py
+++ 2750.py
@@ -1,11 +1,16 @@
 n = int(input())
-list = []
+lst = []
     
 for i in range(n):
     x = int(input())
-    list.append(x)
+    lst.append(x)
 
-list.sort()
+for i in range(n-1):
+    for j in range(n-1-i):
+        if lst[j] > lst[j+1]:
+            temp = lst[j]
+            lst[j] = lst[j+1]
+            lst[j+1] = temp
 
-for i in list:
-    print(i)
+for i in range(n):
+    print(lst[i])
