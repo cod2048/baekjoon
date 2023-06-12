2d2fd443427bc9c0c17ca4fa06765bf6179b91d8
cod2048 <cod2048@gmail.com>
2023-06-12 20:08:45 +0900

    양 한마리...양 두마리..., DFS

---
+++ 11123.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+sys.setrecursionlimit(3000000)
+
+def dfs(y, x):
+    graph[y][x] = '.'
+    for dy, dx in d:
+        Y, X = y+dy, x+dx
+        if (0 <= Y < H) and (0 <= X < W) and graph[Y][X] == '#':
+            dfs(Y, X)
+
+for _ in range(int(input())):
+    H, W = map(int, input().split())
+    graph = [list(input()) for _ in range(H)]
+    d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    cnt = 0
+    for i in range(H):
+        for j in range(W):
+            if graph[i][j] == '#':
+                dfs(i, j)
+                cnt += 1
+    print(cnt)
