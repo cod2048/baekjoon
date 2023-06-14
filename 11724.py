8622a9a64846ea4e73f64f1e3e109484306c1939
cod2048 <cod2048@gmail.com>
2023-06-14 21:07:35 +0900

    연결 요소의 개수, DFS

--- 11724.py
+++ 11724.py
@@ -1,30 +1,27 @@
 import sys
+sys.setrecursionlimit(10000)
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
+
+n, m = map(int, input().split())
+lst = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
 
 def dfs(v):
-    if visited[v] > 0:
-        return
-    
-    visited[v] = 1
-    for i in link[v]:
-        dfs(i)
+    visited[v] = True
+    for i in lst[v]:
+        if not visited[i]:
+            dfs(i)
 
-n,m = map(int,input().split())
-visited = [0] * n
-
-# 인접 리스트 생성
-link = [[] for i in range(n)]
 for _ in range(m):
-    a,b = map(int,input().split())
-    link[a-1].append(b-1)
-    link[b-1].append(a-1)
+    u, v = map(int, input().split())
+    lst[u].append(v)
+    lst[v].append(u)
 
-# r : 연결 요소의 개수
-r = 0
-for i in range(n):
-    if visited[i] == 0:
-        r += 1
+cnt = 0
+
+for i in range(1, n+1):
+    if not visited[i]:
+        cnt += 1
         dfs(i)
-        
-print(r)
+
+print(cnt)
