b6468bd6facef6d6f591103d1d7a2845b28f7efb
cod2048 <cod2048@gmail.com>
2023-06-17 00:48:25 +0900

    n과 m(6), dfs

--- 15655.py
+++ 15655.py
@@ -3,7 +3,21 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-nums = list(map (int, input().split()))
+nums = list(map(int, input().split()))
 
 nums.sort()
+result = []
 
+def dfs(depth, idx):
+    if len(result) == m:
+        print(*result)
+        return
+    for j in range(idx+1, n):
+        result.append(nums[j])
+        dfs(depth+1, j)
+        result.pop()
+
+for i in range(n):
+    result.clear()
+    result.append(nums[i])
+    dfs(1,i)
