123cdb8bfb18382f4dd86e9fdb8c501fb11517a0
cod2048 <cod2048@gmail.com>
2023-06-19 08:16:51 +0900

    수 찾기, 이진탐색

--- 1920.py
+++ 1920.py
@@ -1,27 +1,25 @@
 import sys
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
-a = list(map(int, sys.stdin.readline().split()))
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+num = list(map(int, input().split()))
 
-m = int(sys.stdin.readline())
-b = list(map(int, sys.stdin.readline().split()))
-    
 a.sort()
-    
-def find_num(lst, key):
-    left_key = 0
-    right_key = len(lst)-1
-    while True:
-        mid_num = (left_key+right_key) // 2
-        if lst[mid_num] == key:
-            return 1
-        elif lst[mid_num] < key:
-            left_key = mid_num + 1
-        else:
-            right_key = mid_num - 1
-        if left_key > right_key:
-            return 0
-        
 
-for i in range(len(b)):
-    print(find_num(a, b[i]))
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high)//2
+        if arr[mid] == target:
+            return 1
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid -1
+    return 0
+
+for i in range(m):
+    print(binary_search(a, num[i]))
