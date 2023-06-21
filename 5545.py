5c606f6157d0702a3ce20e4f6b63c6066281b471
cod2048 <cod2048@gmail.com>
2023-06-21 09:28:19 +0900

    최고의 피자, 그리디

---
+++ 5545.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a, b = map(int, input().split())
+c = int(input())
+d = []
+
+for _ in range(n):
+    cal = int(input())
+    d.append(cal)
+
+d.sort(reverse = True)
+cal = c
+result = c / a
+
+for cals in d:
+    new_cal = cal + cals
+    new_price = a + b
+
+    if new_cal / new_price > result:
+        result = new_cal / new_price
+
+        cal = new_cal
+        a = new_price
+    else:
+        break
+
+print(int(result))
+
