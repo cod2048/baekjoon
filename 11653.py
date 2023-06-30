9134b8dc427f947cee5bffdd0288c52adb61b782
cod2048 <cod2048@gmail.com>
2023-06-30 23:22:02 +0900

    소인수분해, 일반수학

---
+++ 11653.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+i = 2
+
+while True:
+    if n == 1:
+        break
+    if n % i == 0:
+        n = n / i
+        print(i)
+    else:
+        i += 1
