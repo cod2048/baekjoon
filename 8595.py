9c0c8d1f2c20f815ab5a946ce220b29673116232
cod2048 <cod2048@gmail.com>
2023-08-19 14:14:39 +0900

    히든넘버, 아스키코드 써서 품

--- 8595.py
+++ 8595.py
@@ -1,15 +1,22 @@
-input()
-s = input()
-hidden_numbers = []
-is_number = False
-num = ""
-for i in s:
-    if i not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-        if len(num) < 7 and len(num) != 0:
-            hidden_numbers.append(int(num))
-        num = ""
-        continue
-    num += i
-if len(num) < 7 and len(num) != 0:
-        hidden_numbers.append(int(num))
-print(sum(hidden_numbers))
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+words = input().strip()
+
+result = 0
+number = ''
+
+for word in words:
+    if ord('0') <= ord(word) <= ord('9'):
+        number += word
+    else:
+        if number == '':
+            continue
+        result += int(number)
+        number = ''
+
+if number != '':
+    result += int(number)
+
+print(result)
