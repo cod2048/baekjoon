81194ffeeab33cd579d2297be940756822af4100
cod2048 <cod2048@gmail.com>
2023-08-23 15:16:52 +0900

    별 찍기, 재귀

--- 2447.py
+++ 2447.py
@@ -1,19 +1,20 @@
 import sys
+input = sys.stdin.readline
 
-def append_star(LEN):
-    if LEN == 1:
+def append_star(length):
+    if length == 1:
         return ['*']
 
-    Stars = append_star(LEN//3) 
-    L = []  
+    stars = append_star(length//3) 
+    l = []  
     
-    for S in Stars:
-        L.append(S*3)
-    for S in Stars:
-        L.append(S+' '*(LEN//3)+S)
-    for S in Stars:
-        L.append(S*3)
-    return L
+    for s in stars:
+        l.append(s*3)
+    for s in stars:
+        l.append(s+' '*(length//3)+s)
+    for s in stars:
+        l.append(s*3)
+    return l
 
-n = int(sys.stdin.readline().strip())
+n = int(input().strip())
 print('\n'.join(append_star(n)))
