d4eb40691be2613a6007cde23eb75c485d174708
cod2048 <cod2048@gmail.com>
2023-09-18 10:36:18 +0900

    직사각형에서 탈출, min

--- 1085.py
+++ 1085.py
@@ -1,13 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 x, y, w, h = map(int, input().split())
 
-a = w-x
-if a >= x:
-    a = x
-b = h-y
-if b >= y:
-    b = y
-
-if a<=b:
-    print(a)
-else:
-    print(b)
+print(min(x, y, w-x, h-y))
