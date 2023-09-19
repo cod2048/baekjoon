44a6d8c38821dc36cb3ccbc574d6bdec74ea9eaa
cod2048 <cod2048@gmail.com>
2023-09-19 10:56:14 +0900

    상수, 문자/정수 변환

--- 2908.py
+++ 2908.py
@@ -1,4 +1,9 @@
-a, b = input().split()
-c = int(a[2]+a[1]+a[0])
-d = int(b[2]+b[1]+b[0])
-print(max(c, d))
+import sys
+input = sys.stdin.readline
+
+A, B = map(str, input().split())
+
+opposite_A = int(A[2]+A[1]+A[0])
+opposite_B = int(B[2]+B[1]+B[0])
+
+print(max(opposite_A, opposite_B))
