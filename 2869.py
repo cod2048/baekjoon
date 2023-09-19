d7efddf7688861c5be6abae77d5d1acef32a2ba0
cod2048 <cod2048@gmail.com>
2023-09-19 11:08:54 +0900

    달팽이는 올라가고 싶다, 소수점 올림

--- 2869.py
+++ 2869.py
@@ -1,5 +1,18 @@
+import sys
 import math
+input = sys.stdin.readline
 
-a, b, v = map(int, input().split())
+A, B, V = map(int, input().split())
 
-print(math.ceil((v-b)/(a-b)))
+# total_distance = 0
+# day = 0
+
+# while True:
+#   day += 1
+#   total_distance += A
+#   if total_distance >= V:
+#     print(day)
+#     break
+#   total_distance -= B
+
+print(math.ceil((V-B)/(A-B)))
