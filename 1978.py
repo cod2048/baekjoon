7d1a7e31e5aa0d30b96cb4693353635a688ee8f9
cod2048 <cod2048@gmail.com>
2023-09-19 11:53:54 +0900

    소수 찾기, 단순 함수이용

--- 1978.py
+++ 1978.py
@@ -1,17 +1,22 @@
+import sys
+input = sys.stdin.readline
+
 def is_prime(n):
-    if n < 2:
+  if n < 2:
+    return False
+  else:
+    for i in range(2, n):
+      if n % i == 0:
         return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
     return True
 
-n = int(input())
-numbers = list(map(int, input().split()))
+N = int(input())
+num_list = list(map(int, input().split()))
+result = 0
 
-count = 0
-for num in numbers:
-    if is_prime(num):
-        count += 1
+for i in num_list:
+  if is_prime(i) == True:
+    result += 1
 
-print(count)
+print(result)
+
