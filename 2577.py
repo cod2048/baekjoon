2ae51a5835c31bbbda129e72e66163b4855e3a55
cod2048 <cod2048@gmail.com>
2023-09-19 10:17:41 +0900

    숫자의 개수, 인덱싱/리스트출력방식

--- 2577.py
+++ 2577.py
@@ -1,3 +1,14 @@
-result = int(input()) * int(input()) * int(input())
-for i in range(0, 10):
-    print(str(result).count(str(i)))
+import sys
+input = sys.stdin.readline
+A = int(input())
+B = int(input())
+C = int(input())
+multiple_num = str(A*B*C)
+result = [0]* 10
+
+for i in range(10):
+  for j in range(len(multiple_num)):
+    if str(i) == multiple_num[j]:
+      result[i] += 1
+
+print(*result, sep='\n')
