3714556dc2f96039b495f8c3273711fe5b4c9509
cod2048 <cod2048@gmail.com>
2023-09-21 16:38:28 +0900

    스택, 스택구현

--- 10828.py
+++ 10828.py
@@ -1,28 +1,28 @@
 import sys
 input = sys.stdin.readline
-stack = []
 
-n= int(input())
+N = int(input())
+stk = []
 
-for i in range(n):
-    x = input().split()
-    if x[0] == 'push':
-        stack.append(x[1])
-    elif x[0] == 'top':
-        if len(stack) == 0:
-            print(-1)
-        else:
-            print(stack[-1])
-    elif x[0] == 'size':
-        print(len(stack))
-    elif x[0] == 'empty':
-        if len(stack) == 0:
-            print(1)
-        else:
-            print(0)
-    elif x[0] == 'pop':
-        if len(stack) == 0:
-            print(-1)
-        else:
-            print(stack[-1])
-            stack.pop()
+for _ in range(N):
+  order = list(map(str, input().split()))
+  if order[0] == 'push':
+    stk.append(order[1])
+  elif order[0] == 'top':
+    if len(stk) == 0:
+      print(-1)
+    else:
+      print(stk[-1])
+  elif order[0] == 'size':
+    print(len(stk))
+  elif order[0] == 'empty':
+    if len(stk) == 0:
+      print(1)
+    else:
+      print(0)
+  elif order[0] == 'pop':
+    if len(stk) == 0:
+      print(-1)
+    else:
+     print(stk.pop())
+  
