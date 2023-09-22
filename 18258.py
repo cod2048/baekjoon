1be6917d1c81d6f24e207e2e9e2bfa395b1e949c
cod2048 <cod2048@gmail.com>
2023-09-22 09:48:44 +0900

    큐2, deque없이 직접 큐 구현

--- 18258.py
+++ 18258.py
@@ -1,33 +1,44 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-n = int(input())
-result = deque([])
+N = int(input())
+queue = [None] * N
+start = 0
+end = 0
 
-for i in range(n):
-    x = input().split()
-    if x[0] == 'push':
-        result.append(x[1])
-    elif x[0] == 'pop':
-        if len(result) == 0:
-            print(-1)
-        else:
-            print(result.popleft())
-    elif x[0] == 'size':
-        print(len(result))
-    elif x[0] == 'empty':
-        if len(result) == 0:
-            print(1)
-        else:
-            print(0)
-    elif x[0] == 'front':
-        if len(result) == 0:
-            print(-1)
-        else:
-            print(result[0])
-    elif x[0] == 'back':
-        if len(result) == 0:
-            print(-1)
-        else:
-            print(result[-1])
+for _ in range(N):
+  order = list(map(str, input().split()))
+  if order[0] == "push":
+    queue[end] = order[1]
+    end += 1
+  elif order[0] == "pop":
+    if end-start:
+      print(queue[start])
+      queue[start] == None
+      start += 1
+    else:
+      print(-1)
+  elif order[0] == "size":
+    print(end-start)
+  elif order[0] == "empty":
+    if end-start:
+      print(0)
+    else:
+      print(1)
+  elif order[0] == "front":
+    if end-start:
+      print(queue[start])
+    else:
+      print(-1)
+  elif order[0] == "back":
+    if end-start:
+      print(queue[end-1])
+    else:
+      print(-1)
+
+# push X : 정수 X를 큐에 넣음
+# pop : 가장 앞에 있는 정수를 빼고 출력,수가 없으면 -1
+# size : 큐에 들어있는 정수의 개수 출력
+# empty : 큐가 비어있으면 1, 아니면 0
+# front : 큐의 가장 앞에 있는 정수를 출력, 정수가 없으면 -1
+# back : 큐의 가장 뒤에 있는 정수를 출력, 정수가 없으면 -1
