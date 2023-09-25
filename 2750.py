90fbb92771cc840fc78d540885c27c346b24b9ac
cod2048 <cod2048@gmail.com>
2023-09-25 13:31:39 +0900

    수 정렬하기, 퀵정렬/재귀

--- 2750.py
+++ 2750.py
@@ -1,16 +1,28 @@
-n = int(input())
-lst = []
-    
-for i in range(n):
-    x = int(input())
-    lst.append(x)
+import sys
+input = sys.stdin.readline
 
-for i in range(n-1):
-    for j in range(n-1-i):
-        if lst[j] > lst[j+1]:
-            temp = lst[j]
-            lst[j] = lst[j+1]
-            lst[j+1] = temp
+def quick_sort(num_lst, left, right):
+  pl = left
+  pr = right
+  pc = (pl + pr) // 2
+  while num_lst[pc]>num_lst[pl]:
+    pl += 1
+  while num_lst[pc]<num_lst[pr]:
+    pr -= 1
+  if pl <= pr:
+    num_lst[pl], num_lst[pr] = num_lst[pr], num_lst[pl]
+    pl += 1
+    pr -= 1
+  if left < pr:
+    quick_sort(num_lst, left, pr)
+  if pl < right:
+    quick_sort(num_lst, pl, right)
+  return num_lst
 
-for i in range(n):
-    print(lst[i])
+N = int(input())
+nums = []
+for _ in range(N):
+  nums.append(int(input()))
+
+for i in quick_sort(nums, 0, N-1):
+  print(i)
